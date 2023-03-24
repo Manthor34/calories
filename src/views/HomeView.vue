552d4817dc243a8073540a3dfa913421402f8790
Manthor34 <lancelot.carrau@gmail.com>
2023-03-24 18:42:46 +0100

    first commit

---
+++ src/views/HomeView.vue
@@ -0,0 +1,18 @@
+<template>
+  <health-form />
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+// Components
+import HealthForm from '../components/HealthForm.vue';
+
+export default defineComponent({
+  name: 'HomeView',
+
+  components: {
+    HealthForm,
+  },
+});
+</script>
